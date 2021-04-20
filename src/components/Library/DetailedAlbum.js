958ddc2a0cd056a385eba9121de56e296e6efa91
AnthonyMCorreia <anthonycorreia12@gmail.com>
2021-04-20 00:13:35 -0700

    search component first outline and autocomplete feature

---
+++ src/components/Library/DetailedAlbum.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const DetailedAlbum = (item) => {
+  const history = useHistory();
+  const { selectedItem } = useSelector((state) => state.library);
+  console.log(selectedItem);
+
+  return <div id="DetailedAlbum"></div>;
+};
+
+export default DetailedAlbum;
